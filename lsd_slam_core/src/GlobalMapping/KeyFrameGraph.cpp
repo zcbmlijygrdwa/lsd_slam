d3887db6cca6d33db7d9dffef581111f825a38f4
Kevin George <kevin@intelligentrobotics.org>
2017-09-13 16:09:06 -0500

    Changes to get library to work with latest G2O

--- lsd_slam_core/src/GlobalMapping/KeyFrameGraph.cpp
+++ lsd_slam_core/src/GlobalMapping/KeyFrameGraph.cpp
@@ -65,17 +65,20 @@ KFConstraintStruct::~KFConstraintStruct()
 KeyFrameGraph::KeyFrameGraph()
 : nextEdgeId(0)
 {
-	typedef g2o::BlockSolver_7_3 BlockSolver;
-	typedef g2o::LinearSolverCSparse<BlockSolver::PoseMatrixType> LinearSolver;
+    typedef g2o::BlockSolver< g2o::BlockSolverTraits<7,3> > BalBlockSolver;
+	typedef g2o::LinearSolverCSparse<BalBlockSolver::PoseMatrixType> BalLinearSolver;
+    std::unique_ptr<g2o::LinearSolver<BalBlockSolver::PoseMatrixType>> linearSolver;
 	//typedef g2o::LinearSolverPCG<BlockSolver::PoseMatrixType> LinearSolver;
-	LinearSolver* solver = new LinearSolver();
-	BlockSolver* blockSolver = new BlockSolver(solver);
-	g2o::OptimizationAlgorithmLevenberg* algorithm = new g2o::OptimizationAlgorithmLevenberg(blockSolver);
+    auto cholesky = g2o::make_unique<BalLinearSolver>();
+    cholesky->setBlockOrdering(true);
+    linearSolver = std::move(cholesky);
+	g2o::OptimizationAlgorithmLevenberg* algorithm = new g2o::OptimizationAlgorithmLevenberg(
+            g2o::make_unique<BalBlockSolver>(std::move(linearSolver)));
 	graph.setAlgorithm(algorithm);
 	
     graph.setVerbose(false); // printOptimizationInfo
-	solver->setWriteDebug(true);
-	blockSolver->setWriteDebug(true);
+	//linearSolver->setWriteDebug(true);
+	//blockSolver->setWriteDebug(true);
 	algorithm->setWriteDebug(true);
 
 
